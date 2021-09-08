d13de1af791a657628a8a945e74e137fb6f56def
Patrick Wu <Ptrckwu@yahoo.com>
2021-09-07 20:01:27 -0400

    commit tree 387f8ab8d37cf7b3aeede57f18361e72e136ab1b

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((data) => renderBooks(data))
+} //console logs the n'th book in collection
 
 function renderBooks(books) {
   const main = document.querySelector('main');
